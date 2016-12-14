018b202143c674a05085559d444f0f10dd09c9b5
Przemysław Olech <przemyslaw.olech@gmail.com>
2016-12-14 22:59:50 +0100

    Update gradientDescentMulti.m

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -17,6 +17,7 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+ % This solution works also for gradientDescent.m
 
   Hx = X*theta;
   dt  = (X'*(Hx-y));
